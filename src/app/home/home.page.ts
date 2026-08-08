0aa8559fd5430619f017a56f169e31d2158f777a
Mac <mac@192.168.1.10>
2026-08-07 20:10:20 -0500

    feat: implementada gestion de tareas, categorias y almacenamiento local

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { Storage } from '../services/storage';
 import { Task } from '../models/task';
+import { Category } from '../models/category';
+import { AlertController } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
@@ -15,45 +17,96 @@ import { Task } from '../models/task';
 
 export class HomePage implements OnInit {
   tasks: Task[] = [];
+  categories: Category[] = [];
+  
+  // Variables para la interfaz
   newTaskTitle: string = '';
+  newTaskCategoryId: string = ''; 
+  filterCategoryId: string = 'all'; 
 
-  constructor(private storageService: Storage) {}
+  constructor(
+    private storageService: Storage,
+    private alertController: AlertController
+  ) {}
 
   ngOnInit() {
-    this.loadTasks();
+    this.cargarData();
   }
 
- 
-  loadTasks() {
+  cargarData() {
     this.tasks = this.storageService.obtenerTarea();
     this.tasks.sort((a, b) => b.createdAt - a.createdAt);
+    this.categories = this.storageService.obtenerCategorias();
   }
 
+  get filteredTasks() {
+    if (this.filterCategoryId === 'all') return this.tasks;
+    return this.tasks.filter(t => t.categoryId === this.filterCategoryId);
+  }
 
-  addTask() {
+  anadirTarea() {
     if (this.newTaskTitle.trim().length === 0) return;
 
     const newTask: Task = {
       id: this.storageService.generarId(),
       title: this.newTaskTitle.trim(),
       completed: false,
-      createdAt: Date.now()
+      createdAt: Date.now(),
+      categoryId: this.newTaskCategoryId || undefined
     };
 
-    this.tasks.unshift(newTask); 
-    this.storageService.guardarTarea(this.tasks); 
-    this.newTaskTitle = ''; 
+    this.tasks.unshift(newTask);
+    this.storageService.guardarTarea(this.tasks);
+    
+    // Limpiamos los inputs
+    this.newTaskTitle = '';
+    this.newTaskCategoryId = ''; 
   }
 
- 
   toggleComplete(task: Task) {
     task.completed = !task.completed;
     this.storageService.guardarTarea(this.tasks);
   }
 
- 
-  deleteTask(taskId: string) {
+  eliminarTarea(taskId: string) {
     this.tasks = this.tasks.filter(t => t.id !== taskId);
     this.storageService.guardarTarea(this.tasks);
+  }
+
+ 
+  async anCategory() {
+    const alert = await this.alertController.create({
+      header: 'Nueva Categoría',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'Ej. Trabajo, Hogar etc..'
+        }
+      ],
+      buttons: [
+        { text: 'Cancelar', role: 'cancel' },
+        {
+          text: 'Guardar',
+          handler: (data:any) => {
+            if (data.name.trim()) {
+              const newCat: Category = {
+                id: this.storageService.generarId(),
+                name: data.name.trim()
+              };
+              this.categories.push(newCat);
+              this.storageService.guardarCategorias(this.categories);
+            }
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  obtenerNombreCategoria(id?: string): string {
+    if (!id) return '';
+    const cat = this.categories.find(c => c.id === id);
+    return cat ? cat.name : '';
   }
 }
