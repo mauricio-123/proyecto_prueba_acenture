b70aaf012a76872b1d7408c5d2b749d91abf5897
Mac <mac@192.168.1.10>
2026-08-08 19:45:06 -0500

    Agregar iconos mejorar la visivilidad de la pagina y hacer las amigable

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,39 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {
+  RouteReuseStrategy,
+  provideRouter
+} from '@angular/router';
+
+import {
+  IonicRouteStrategy,
+  provideIonicAngular
+} from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+import {
+  add,
+  addCircleOutline,
+  trash
+} from 'ionicons/icons';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
+
+addIcons({
+  add,
+  addCircleOutline,
+  trash
+});
+
+
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    {
+      provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy
+    },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes)
   ],
 });
